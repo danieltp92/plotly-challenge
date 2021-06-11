dfdc542e645530ec18f2747cd6a4a1d057dc5b38
danieltp92 <daniel.tp92@gmail.com>
2021-06-10 22:53:51 -0500

    design functions

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,58 +1,72 @@
 
+function fillCharts(){
+    d3.json("samples.json").then((data) => {
 
-let value = sample.sample_values;
-let id = sample.otu_ids;
-let label = smaple.otu_labels;
+        let value = sample.sample_values;
+        let id = sample.otu_ids;
+        let label = smaple.otu_labels;
 
-let chart = {
-    y: id,
-    x: value,
-    text: label,
-    marker:{
-        color: ['rgba(222,45,38,0.8)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)']
-    },
-    type:"bar",
-    orientation:"h"
-}
+        let chart = {
+            y: id,
+            x: value,
+            text: label,
+            marker:{
+                color: ['rgba(222,45,38,0.8)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)', 'rgba(49,130,189,1)']
+            },
+            type:"bar",
+            orientation:"h"
+        }
 
-let bar_chart = [chart];
+        let bar_chart = [chart];
 
-let barChart = {
-    title: "10 most founded bacteria",
-    xaxis: {title: "Samples Values"},
-    yaxis: {title: "Bacteria ID"}
-}
+        let barChart = {
+            title: "10 most founded bacteria",
+            xaxis: {title: "Samples Values"},
+            yaxis: {title: "Bacteria ID"}
+        }
 
-Plotly.newPlot("bar", bar_data, barLayout)
+        Plotly.newPlot("bar", bar_data, barLayout)
 
 
-let bubbles = {
-    x: id,
-    y: value,
-    text: label,
-    mode: "markers",
-    marker: {
-        color: id,
-        size: value
-    }
-}
+        let bubbles = {
+            x: id,
+            y: value,
+            text: label,
+            mode: "markers",
+            marker: {
+                color: id,
+                size: value
+            }
+        }
 
-let bubble_chart = [bubbles];
+        let bubble_chart = [bubbles];
 
-let bubbleChart = {
-    xaxis: {title: "Bacteria IDs"},
-    yaxis: {title: "Samples Values"}
-}
+        let bubbleChart = {
+            xaxis: {title: "Bacteria IDs"},
+            yaxis: {title: "Samples Values"}
+        };
 
-Plotly.newPlot("bubble", bubbles, bubbleChart)
+        Plotly.newPlot("bubble", bubbles, bubbleChart)
 
-let dropMenu = d3.select("#selDataset");
-
-d3.json("samples.json").then(function(data){
-    let optionId = data.names;
-    optionId.forEach(value => {
-        dropMenu.append("option").text(value).property("value", value)
     });
 
-    
-});
+}
+
+function initial(){
+    let dropMenu = d3.select("#selDataset");
+
+    d3.json("samples.json").then(function(data){
+        let optionId = data.names;
+        optionId.forEach(value => {
+            dropMenu.append("option").text(value).property("value", value)
+        });
+        
+    });
+
+}
+
+function changeOption(){
+    fillCharts();
+}
+
+initial();
